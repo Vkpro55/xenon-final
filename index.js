4a33488aae55823f85231a3370e0eb7aca8b53e6
Vinod Kumar <vinodrao835@gmail.com>
2023-12-30 04:35:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,105 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+const username = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD;
+mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.jhjublv.mongodb.net/registrationFormDB`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+const registrationSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+})
+
+// model
+const Registration = mongoose.model("Registration", registrationSchema);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/pages/index.html")
+})
+
+app.post("/register", async(req, res) => {
+    try {
+         // destructing
+        const { name, email, password } = req.body;
+        
+        const existUser = await Registration.findOne({ email: email });
+        if (!existUser) {
+            // save it to mongodb
+            const registrationData = new Registration({
+            name,
+            email,
+            password
+        });
+            await registrationData.save();
+            res.redirect("/success")
+            
+        }
+        else {
+            console.log("user already exist");
+            res.redirect("/error")
+        }
+        
+    }
+    catch (error) {
+        console.log(error);
+        res.redirect("/error")
+    }
+})
+
+app.post("/bikers", async(req, res) => {
+    try {
+         // destructing
+        const { name, email, password } = req.body;
+        
+        const existUser = await Registration.findOne({ email: email });
+        if (!existUser) {
+            // save it to mongodb
+            const registrationData = new Registration({
+            name,
+            email,
+            password
+        });
+            await registrationData.save();
+            res.redirect("/biker")
+            
+        }
+        else {
+            console.log("user already exist");
+            res.redirect("/error")
+        }
+        
+    }
+    catch (error) {
+        console.log(error);
+        res.redirect("/error")
+    }
+})
+
+app.get("/success", (req, res) => {
+    res.sendFile(__dirname + "/pages/success.html");
+})
+
+app.get("/error", (req, res) => {
+    res.sendFile(__dirname + "/pages/error.html");
+})
+
+app.get("/biker", (req, res) => {
+    res.sendFile(__dirname + "/pages/biker.html");
+})
+
+app.listen(port, (req, res) => {
+    console.log(`server is running on port ${port}`);
+})
